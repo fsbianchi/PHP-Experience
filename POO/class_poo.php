9e28371472200b3f0abf29ec81b6aa60e723d8e4
Felipe Bianchi <157925798+fsbianchi@users.noreply.github.com>
2025-06-24 23:23:59 -0300

    Add files via upload

---
+++ POO/class_poo.php
@@ -0,0 +1,15 @@
+<?php
+
+class ContaBancaria{
+
+}
+new ContaBancaria();// declaração de um objeto da classe ContaBancaria
+// A classe ContaBancaria foi declarada, mas não possui propriedades ou métodos.
+
+$conta = new ContaBancaria(); // $conta é uma instância da classe ContaBancaria
+// A variável $conta agora contém um objeto da classe ContaBancaria.
+
+var_dump($conta); // Exibe informações sobre o objeto $conta
+// O var_dump mostrará que $conta é um objeto do tipo ContaBancaria e suas propriedades e métodos (se existirem) serão exibidos.
+
+?>;
---
+++ POO/php_poo.php
@@ -0,0 +1,9 @@
+<?php
+
+class Computador
+{
+
+}
+
+$computador = new Computador();
+var_dump($computador);
